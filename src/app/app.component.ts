70921e44bea6cdcab7d219e4fef0f1d70f892402
hp <developers@instaspiel.com>
2018-08-07 14:28:02 +0530

    dfsd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {RequestOptions,Http, Request, RequestMethod} from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'appli';
+export class AppComponent implements OnInit {
+  title = 'applidvcfdfsd';
+  constructor(private http: Http) {
+const headers = new Headers();
+headers.append('Access-Control-Allow-Headers', 'Content-Type');
+headers.append('Access-Control-Allow-Methods', 'GET');
+headers.append('Access-Control-Allow-Origin', '*');
+
+  }
+  ngOnInit(){
+
+
+}
 }
